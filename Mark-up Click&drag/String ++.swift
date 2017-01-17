43f57ae33d6c79dd623a6ee5a470090766760371
Qualan Woodard <quawood@qualans-imac.local>
2017-01-16 19:23:59 -0500

    test

---
+++ Mark-up Click&drag/String ++.swift
@@ -0,0 +1,136 @@
+//
+//  User.swift
+//  Mark-up Click&drag
+//
+//  Created by Qualan Woodard on 8/28/16.
+//  Copyright © 2016 Pushbox, LLC'. All rights reserved.
+//
+
+import UIKit
+
+
+
+
+// 1
+class AllowedCharsTextField: UITextField, UITextFieldDelegate {
+    
+    // 2
+    @IBInspectable var allowedChars: String = ""
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        // 3
+        delegate = self
+        // 4
+        autocorrectionType = .no
+    }
+    
+    // 5
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        // 6
+        guard string.characters.count > 0 else {
+            return true
+        }
+        
+        // 7
+        let currentText = textField.text ?? ""
+        let prospectiveText = (currentText as NSString).replacingCharacters(in: range, with: string)
+        return prospectiveText.containsOnlyCharactersIn(matchCharacters: allowedChars)
+    }
+    
+}
+
+
+// 8
+extension String {
+    
+    // Returns true if the string contains only characters found in matchCharacters.
+    func containsOnlyCharactersIn(matchCharacters: String) -> Bool {
+        let disallowedCharacterSet = NSCharacterSet(charactersIn: matchCharacters).inverted
+        return self.rangeOfCharacter(from: disallowedCharacterSet) == nil
+    }
+    
+}
+
+extension Array {
+    func rotate(shift:Int) -> Array {
+        var array = Array()
+        if (self.count > 0) {
+            array = self
+            if (shift > 0) {
+                for _ in 1...shift {
+                    array.append(array.remove(at: 0))
+                }
+            }
+            else if (shift < 0) {
+                for _ in 1...abs(shift) {
+                    array.insert(array.remove(at: array.count-1),at:0)
+                }
+            }
+        }
+        return array
+    }
+}
+
+
+extension String {
+    
+    subscript (i: Int) -> Character {
+        return self[self.index(startIndex, offsetBy: i)]
+    }
+    
+}
+
+public class LoadingOverlay{
+    
+    var overlayView = UIView()
+    var activityIndicator = UIActivityIndicatorView()
+    
+    class var shared: LoadingOverlay {
+        struct Static {
+            static let instance: LoadingOverlay = LoadingOverlay()
+        }
+        return Static.instance
+    }
+    
+    public func showOverlay(view: UIView) {
+        
+        overlayView.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
+        overlayView.center = view.center
+        overlayView.backgroundColor = UIColor(white: 0x444444, alpha: 0.7)
+        overlayView.clipsToBounds = true
+        overlayView.layer.cornerRadius = 10
+        
+        activityIndicator.frame = CGRect(x:0, y:0, width:40, height:40)
+        activityIndicator.activityIndicatorViewStyle = .whiteLarge
+        activityIndicator.center = CGPoint(x:overlayView.bounds.width / 2, y:overlayView.bounds.height / 2)
+        
+        overlayView.addSubview(activityIndicator)
+        view.addSubview(overlayView)
+        
+        activityIndicator.startAnimating()
+    }
+    
+    public func hideOverlayView() {
+        activityIndicator.stopAnimating()
+        overlayView.removeFromSuperview()
+    }
+}
+
+protocol LoadingViewControllerDelegate {
+    func didFinishLoading(controller: LoadingViewController)
+}
+
+class LoadingViewController: UIViewController {
+    var delegate: LoadingViewControllerDelegate? = nil
+    
+    override func viewDidLoad() {
+        // Do your spinner code here
+    }
+    
+    // Delegate function, should be fired when loading is done.
+    func finished() {
+        if self.delegate != nil {
+            self.delegate?.didFinishLoading(controller: self)
+        }
+    }
+}
